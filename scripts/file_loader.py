4512188f6c929eeaa15638c11d895dafddd3a6a4
Paul Chappus <paul@Pauls-MacBook-Pro-2.local>
2024-10-13 14:18:23 -0400

    add custom file loading logic for files 1 - 3

--- scripts/file_loader.py
+++ scripts/file_loader.py
@@ -42,11 +42,11 @@ class FileLoader:
 
     def load_file(self, sheet_name=None):
         """main method to load file based on its type."""
-        if self.file_name == 'file_one':
+        if self.file_name.startswith('file_one'):
             return self._load_csv_file() # assume this one is a csv
-        elif self.file_name == 'file_two':
+        elif self.file_name.startswith('file_two'):
             return self._load_file_two(sheet_name)
-        elif self.file_name == 'file_three':
+        elif self.file_name.startswith('file_three'):
             return self._load_file_three(sheet_name)
         else:
             raise ValueError(f"Unknown file type: {self.file_name}. Ensure file has file_loader logic.")
@@ -55,7 +55,6 @@ class FileLoader:
         """Logic required to read in file one."""
         try: 
             df = pd.read_csv(self.file_path)
-            # put the custom logic here
             return df
         except Exception as e:
             print(f"Error loading {self.file_name}: {e}")
@@ -63,22 +62,62 @@ class FileLoader:
         
     def _load_file_two(self, sheet_name):
         """Logic required to read in file two."""
-        try: 
-            df = pd.read_excel(self.file_path, sheet_name = sheet_name)
-            # put the custom logic here
-            return df
-        except Exception as e:
-            print(f"Error loading {self.file_name}: {e}")
+        if sheet_name == "Current":
+            try: 
+                df = pd.read_excel(self.file_path, sheet_name = sheet_name, skiprows = 4)
+                df.drop(index = 5, inplace = True) # drop the blank row of data
+                df.reset_index(drop = True, inplace = True)
+                df.rename(columns={df.columns[0]: 'element_type'}, inplace = True)
+                return df
+            except Exception as e:
+                print(f"Error loading {self.file_name}: {e}")
+                return None
+        elif sheet_name == "Diff":
+            try: 
+                df = pd.read_excel(self.file_path, sheet_name = sheet_name, header=0, skiprows=range(1,4))
+                df.rename(columns={df.columns[0]: 'group_type'}, inplace = True)
+                return df
+            except Exception as e:
+                print(f"Error loading {self.file_name}, tab {sheet_name}: {e}")
+                return None
+        else:
+            print(f"Error loading {self.file_name}, tab {sheet_name}. Sheet name logic not included in _load_file_three.")
             return None
         
     def _load_file_three(self, sheet_name):
         """Logic required to read in file three."""
         try: 
-            df = pd.read_excel(self.file_path, sheet_name = sheet_name)
-            # put the custom logic here
+            df = pd.read_excel(self.file_path, sheet_name = sheet_name, skiprows=5)
+            
+            # there is data in column 1 that are "groups". Take those and make them a data element on each row it pertains to.
+            rows_with_identifiers = []
+            current_identifer = None
+
+            # TODO fix logic so that the identifers get placed as a new data column
+            #      identifers are specific to certain rows of data. 
+            #      
+            
+            # iterate through rows
+            """
+            for index, row in df.iterrows():
+                identifer = row[0]
+
+                if pd.notna(identifer):
+                    current_identifer = identifer
+                else:
+                    if current_identifer is None:
+                        continue
+
+                rows_with_identifiers.append([current_identifer] + row.tolist())
+
+            identifer_df = pd.DataFrame(rows_with_identifiers)
+            """
+            # delete 2nd column which is empty
+            df.drop(df.columns[1], axis=1, inplace=True)
+
             return df
         except Exception as e:
-            print(f"Error loading {self.file_name}: {e}")
+            print(f"Error loading {self.file_name}, tab {sheet_name}: {e}")
             return None
         
     def _load_file_four(self, sheet_name):
